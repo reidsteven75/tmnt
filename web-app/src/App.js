fc9bc2197dda8678e86f33125deb898d64d3d963
reidsteven75 <reidsteven75@gmail.com>
2020-03-12 20:07:58 -0600

    visualization animation

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import blue from '@material-ui/core/colors/blue'
+import teal from '@material-ui/core/colors/teal'
+
 import Header from './components/header'
 import Viewer from './components/viewer'
 
@@ -9,6 +14,67 @@ const PROD = (process.env.ENV === 'production')
 const SERVER = (HTTPS ? 'https://' : 'http://') + process.env.HOST + (PROD ? '' : ':' + process.env.PORT_SERVER)
 const API = SERVER + '/api'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: blue[300],
+      main: blue[500],
+      dark: blue[700],
+    },
+    secondary: {
+      light: teal['A200'],
+      main: teal['A400'],
+      dark: teal['A700'],
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
+
+const config = {
+  gridDimensions: 20,
+  simulateSpeed: {
+    fast: {
+      updateRate: 50,
+      easing: 0.95
+    },
+    medium: {
+      updateRate: 400,
+      easing: 0.2
+    },
+    slow: {
+      updateRate: 2000,
+      easing: 0.05
+    }
+  }
+}
+
+const simulateSpeed = config.simulateSpeed['medium']
+const gridDimensions = config.gridDimensions
+
+const steps = [
+  {char: 'start', trans: { x:0, y:0 }, rot: 0},
+  {char: 'L', dupNode: false, dupPath: false, position: { x:0, y:0 }, rotation: 270},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:0, y:0 }, rotation: 0},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:0, y:0 }, rotation: 90},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:1, y:0 }, rotation: 90},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:2, y:0 }, rotation: 90},
+  {char: 'L', dupNode: false, dupPath: false, position: { x:2, y:0 }, rotation: 0},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:2, y:-1 }, rotation: 0},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:2, y:-1 }, rotation: 90},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:2, y:-1 }, rotation: 180},
+  {char: 'F', dupNode: true, dupPath: true, position: { x:2, y:0 }, rotation: 180},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:2, y:0 }, rotation: 270},
+  {char: 'F', dupNode: true, dupPath: true, position: { x:1, y:0 }, rotation: 270},
+  {char: 'R', dupNode: false, dupPath: false, position: { x:1, y:0 }, rotation: 0},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:1, y:-1 }, rotation: 0},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:1, y:-2 }, rotation: 0},
+  {char: 'F', dupNode: false, dupPath: false, position: { x:1, y:-3 }, rotation: 0}
+]
+
+
 // app
 // ----
 class App extends Component {
@@ -16,16 +82,62 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      stepCurrent: null,
+      stepPrevious: null,
+      stepNext: null,
+      stepIndexNext: 0
     }
   }
+
+  componentDidMount() {
+
+    let index = 0
+    setTimeout(() => {
+      setInterval(() => {
+
+        let stepCurrent, stepNext, stepPrevious
+  
+        if (typeof steps[index] !== 'undefined') { stepCurrent = steps[index] }
+        if (typeof steps[index + 1] !== 'undefined') { stepNext = steps[index + 1] }
+        if (typeof steps[index - 1] !== 'undefined') { stepPrevious = steps[index - 1] }
+  
+        if (stepNext) {
+          this.setState({
+            stepCurrent: stepCurrent,
+            stepNext: stepNext,
+            stepPrevious: stepPrevious,
+            stepIndexNext: index + 1
+          })
+        }
+
+        index += 1
+      }, simulateSpeed.updateRate)
+    }, 1000)
+    
+  }
+
   render() {
 
+    const { stepPrevious, 
+            stepCurrent, 
+            stepNext, 
+            stepIndexNext } = this.state
+
     return (
-      <div className='App'>
-        <Header/>
-        <Viewer/>
-      </div> 
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='App'>
+          <Header/>
+          <Viewer
+            easing={simulateSpeed.easing}
+            gridDimensions={gridDimensions}
+            stepCurrent={stepCurrent}
+            stepPrevious={stepPrevious}
+            stepNext={stepNext}
+            stepIndexNext={stepIndexNext}
+          />
+        </div> 
+      </MuiThemeProvider>
     )
   }
 }
--- web-app/src/components/viewer/index.js
+++ web-app/src/components/viewer/index.js
@@ -1,33 +1,381 @@
 import React, { Component } from 'react'
+import * as p5 from 'p5'
 
 const style = {
-
+	wrapper: {
+		padding: 15,
+		height: '100%',
+		margin: 'auto'
+	},
+	sketchArea: {
+		border: '1px solid black',
+		height: '90vh',
+		margin: 'auto'
+	}
 }
 
+/*
+	rotation: 0-359 degrees, 0 = north
+*/
+
 class Viewer extends Component {
+
+	config = {
+		circleRadius: 25,
+		triangleSize: 20,
+		updateInterval: 500,
+		nodeRadiusUnique: 5,
+		nodeRadiusDuplicate: 15,
+	}
+
+	cache = {
+		nodesTravelled: [],
+		pathsTravelled: [],
+		nodesDuplicate: [],
+		pathsDuplicate: [],
+	}
+
+	// lifecycle
+	// ---------
 
   constructor(props) {
     super(props)
     this.state = {
-			loading: true
+			loading: true,
+			stepSizePixels_X: 0,
+			stepSizePixels_Y: 0,
+			canvasWidth: 0,
+			canvasHeight: 0,
+			origin_X: 0,
+			origin_Y: 0,
+			prevIndex: 0,
+			prevPosition_X: 0,
+			prevPosition_Y: 0,
+			prevRotation: 0,
+			currentIndex: 0,
+			currentPosition_X: 0,
+			currentPosition_Y: 0,
+			currentRotation: 0,
+			nextIndex: 0,
+			nextPosition_X: 0,
+			nextPosition_Y: 0,
+			nextRotation: 0
 		}
+
+		this.sketchRef = React.createRef()
+		
 	}
 
 	componentDidMount() {
+
+		this.s = (sk) => {  
+			this.sk = sk
+			this.updateCanvasSize()
+			this.initP5()
+		}
+		this.p5 = new p5(this.s)
 		this.setState({loading:false})
+
 	}	
-  
+
+	componentDidUpdate(props) {
+
+		const { nextIndex, 
+						currentPosition_X, 
+						currentPosition_Y, 
+						stepSizePixels_X, 
+						stepSizePixels_Y,
+						origin_X,
+						origin_Y } = this.state,
+
+					{ stepNext, stepIndexNext } = props,
+
+					{ nodesTravelled,
+						pathsTravelled,
+						nodesDuplicate,
+						pathsDuplicate } = this.cache
+
+		// check if positions should be re-calculated
+		if (nextIndex !== stepIndexNext) {
+
+			// position & rotation
+			const nextPosition_X = origin_X + stepSizePixels_X * stepNext.position.x,
+			 			nextPosition_Y = origin_Y + stepSizePixels_Y * stepNext.position.y,
+			 			nextRotation = stepNext.rotation
+
+			// nodes					
+			if (stepNext.dupNode === true) { 
+				nodesDuplicate.push({
+					x:currentPosition_X, 
+					y:currentPosition_Y 
+				}) 
+			}
+			else {
+				nodesTravelled.push({
+					x:currentPosition_X, 
+					y:currentPosition_Y 
+				})
+			}
+
+			// paths
+			if (stepNext.dupPath === true) { 
+				pathsDuplicate.push({
+					x1:currentPosition_X, 
+					y1:currentPosition_Y,
+					x2:nextPosition_X, 
+					y2:nextPosition_Y
+				}) 
+			}
+			else {
+				pathsTravelled.push({
+					x1:currentPosition_X, 
+					y1:currentPosition_Y,
+					x2:nextPosition_X, 
+					y2:nextPosition_Y
+				}) 
+			}
+
+			this.setState({
+				nextIndex: stepIndexNext,
+				nextPosition_X: nextPosition_X,
+				nextPosition_Y: nextPosition_Y,
+				nextRotation: nextRotation,
+			})
+		}
+	}
+
+	// functions
+	// ---------
+
+	updateCanvasSize() {
+		const height = this.sketchRef.current.offsetHeight
+		const width = this.sketchRef.current.offsetWidth
+		this.setState({
+			canvasWidth: width,
+			canvasHeight: height
+		})
+	}
+
+	redrawCanvas() {
+
+		const { canvasWidth, 
+						canvasHeight, 
+						currentPosition_X, 
+						currentPosition_Y, 
+						nextPosition_X, 
+						nextPosition_Y } = this.state
+
+		this.setState({
+			currentPosition_X: currentPosition_X + diffX,
+			currentPosition_Y: currentPosition_Y + diffY,
+			nextPosition_X: nextPosition_X + diffX,
+			nextPosition_Y: nextPosition_Y + diffY
+		}, () => {
+			this.p5.resizeCanvas(canvasWidth, canvasHeight).redraw()
+		})
+	}
+
+	initP5() {
+
+		const { gridDimensions } = this.props
+
+		this.sk.setup = () => {
+
+			const { canvasWidth, canvasHeight } = this.state
+
+			const sketchRef = this.sketchRef.current
+			const origin_X = sketchRef.offsetWidth/2
+			const origin_Y = sketchRef.offsetHeight/2
+
+			let canvas = this.sk.createCanvas(canvasWidth, canvasHeight)
+			this.sk.noStroke()
+			canvas.parent(sketchRef.id)
+
+			this.setState({
+				origin_X: origin_X,
+				origin_Y: origin_Y,
+				currentPosition_X: origin_X,
+				currentPosition_Y: origin_Y,
+				nextPosition_X: origin_X,
+				nextPosition_Y: origin_Y,
+			})
+
+		}
+
+		this.sk.draw = () => {
+
+			const { canvasWidth, 
+							canvasHeight, 
+							nextPosition_X, 
+							nextPosition_Y, 
+							currentPosition_X, 
+							currentPosition_Y, 
+							currentRotation,
+							nextRotation } = this.state,
+
+						{ easing } = this.props,
+
+						{ nodesTravelled,
+							nodesDuplicate,
+							pathsTravelled,
+							pathsDuplicate } = this.cache,
+
+						{ circleRadius,
+							triangleSize,
+							nodeRadiusUnique,
+							nodeRadiusDuplicate } = this.config,
+						
+						cos = (degrees) => { return(Math.cos(degrees * Math.PI / 180).toFixed(2)) },
+						sin = (degrees) => { return(Math.sin(degrees * Math.PI / 180).toFixed(2)) }
+
+			var	dx, 
+					dy,
+					drot,
+					colorIntensity = 0,
+					colorIncrement = 1
+
+			this.sk.clear().noFill()
+
+			// grid
+			for (var x = 0; x < canvasWidth; x += canvasWidth / gridDimensions) {
+				for (var y = 0; y < canvasHeight; y += canvasHeight / gridDimensions) {
+					this.sk.stroke(105,105,105);
+					this.sk.strokeWeight(0.1);
+					this.sk.line(x, 0, x, canvasHeight);
+					this.sk.line(0, y, canvasWidth, y);
+				}
+			}
+			
+			// turtle
+			for (var i = 0; i < circleRadius; i += colorIncrement ) {
+
+				// determine if moving
+				if (Math.abs(dy) > 0.5 || Math.abs(dx) > 0.5) {
+					colorIntensity = Math.floor(Math.random() * 5 * Math.max(Math.abs(dx),Math.abs(dy))) 
+					colorIncrement = 2
+					this.sk.stroke(37 + colorIntensity, 175 + colorIntensity, 180 + colorIntensity)
+					this.sk.strokeWeight(1)
+				}
+				else { 
+					colorIncrement = 5
+					this.sk.stroke(37 + colorIntensity, 175 + colorIntensity, 180 + colorIntensity)
+					this.sk.strokeWeight(3)
+				}
+
+				const x1 = currentPosition_X - (triangleSize/2)*cos(currentRotation),
+							x2 = currentPosition_X + (triangleSize/2)*cos(currentRotation),
+							x3 = currentPosition_X + triangleSize*sin(currentRotation),
+							y1 = currentPosition_Y - (triangleSize/2)*sin(currentRotation),
+							y2 = currentPosition_Y + (triangleSize/2)*sin(currentRotation),
+							y3 = currentPosition_Y - triangleSize*cos(currentRotation)
+
+				this.sk.triangle(x1,y1,x2,y2,x3,y3)
+								.ellipse(
+									currentPosition_X,
+									currentPosition_Y,
+									i,
+									i
+								)
+			}	
+
+			// travelled paths
+			if (pathsTravelled.length > 0) {
+				pathsTravelled.forEach((path) => {
+					this.sk.stroke(	152, 255, 152)
+								.strokeWeight(1)
+								.line(
+									path.x1,
+									path.y1,
+									path.x2,
+									path.y2
+								)
+				})
+			}
+			// duplicate paths
+			if (pathsDuplicate.length > 0) {
+				pathsDuplicate.forEach((path) => {
+					this.sk.stroke(	249, 111, 97)
+								.strokeWeight(4)
+								.line(
+									path.x1,
+									path.y1,
+									path.x2,
+									path.y2
+								)
+				})
+			}
+			
+			// travelled nodes
+			if (nodesTravelled.length > 0) {
+				nodesTravelled.forEach((node) => {
+					this.sk.stroke(	152, 255, 152)
+								.strokeWeight(1)
+								.ellipse(
+									node.x,
+									node.y,
+									nodeRadiusUnique,
+									nodeRadiusUnique
+								)
+				})
+			}
+			// duplicate nodes
+			if (nodesDuplicate.length > 0) {
+				nodesDuplicate.forEach((node) => {
+					this.sk.stroke(	249, 111, 97)
+								.strokeWeight(1)
+								.ellipse(
+									node.x,
+									node.y,
+									nodeRadiusDuplicate,
+									nodeRadiusDuplicate
+								)
+				})
+			}
+
+			// update position
+
+			dy = nextPosition_Y - currentPosition_Y
+			dx = nextPosition_X - currentPosition_X
+			
+			// TODO: figure out rotation edge cases below
+			// if (currentRotation <= 5 && currentRotation >= 0 && nextRotation === 270) {
+			// 	currentRotation = 360
+			// }
+			// else if (currentRotation <= 5 && currentRotation >= 0 && nextRotation === 0) {
+			// 	drot = 90
+			// }
+			// else {
+				drot = nextRotation - currentRotation
+			// }
+
+			this.setState({
+				stepSizePixels_X: canvasWidth / gridDimensions,
+				stepSizePixels_Y: canvasHeight / gridDimensions,
+				currentRotation: currentRotation + drot * easing,
+				currentPosition_X: currentPosition_X + dx * easing,
+				currentPosition_Y: currentPosition_Y + dy * easing
+			})
+		}
+	}
+	
+	// render
+	// ------
+
   render() {
-		const content = 
-				<div style={style.viewer}>
 
-				</div>
-
-    return (
-			<div style={style.content}>
-				{ content }
+		let content = 
+			<div 
+				id='sketch-area' 
+				ref={this.sketchRef}
+				style={style.sketchArea}
+			>
 			</div>
-    )
+      
+    return (
+        <div style={style.wrapper}>
+					{content}
+        </div>
+		)
   }
 }
 
